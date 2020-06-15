96b0a94079b78b3e0d30bc728de9091df6f6e37a
tim <timajwilliams@gmail.com>
2020-06-15 08:11:56 +0100

    add repo for edit, remove public

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,7 @@ module.exports = {
       options: {
         contentPath: 'notes',
         basePath: '/',
+        gitRepoContentPath:'https://github.com/timajwilliams/snippets/tree/master/notes',
         showThemeInfo: false,
         showDescriptionInSidebar: true,
       },
